9fee29cfd8e10ff1fc2ec0459bd1fcd6fb04236b
coralian <twwamidgar@gmail.com>
2024-04-09 23:52:09 -0400

    Update hubtool.rb

--- Casks/hubtool.rb
+++ Casks/hubtool.rb
@@ -3,7 +3,7 @@ cask "hubtool" do
   sha256 "35ef3b9bbdad82bf18b593e12f47fae057cc96d93cc99b774e62d1dd90d2d5c6"
   url "https://acroname.com/sites/default/files/software/hubtool/#{version}/HubTool_#{version}_macx_universal.dmg"
   name "AcronameHubTool"
-  desc "Software for Acroname products"
+  desc "Hub tool software for Acroname USB products"
   homepage "https://acroname.com/software/hubtool"
 
   app "HubTool.app"
