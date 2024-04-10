cce247e5b2fd5c0ee6994cca7393e0dbb378f00f
coralian <twwamidgar@gmail.com>
2024-04-09 23:47:47 -0400

    Update and rename acronameHubTool.rb to hubtool.rb

--- Casks/hubtool.rb
+++ Casks/hubtool.rb
@@ -1,4 +1,4 @@
-cask "acronameHubTool" do
+cask "hubtool" do
   version "2.10.6"
   sha256 "35ef3b9bbdad82bf18b593e12f47fae057cc96d93cc99b774e62d1dd90d2d5c6"
   url "https://acroname.com/sites/default/files/software/hubtool/#{version}/HubTool_#{version}_macx_universal.dmg"
